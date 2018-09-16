d933caee213e5b45f2ed6dcd24f4cd47f8d5d04b
Kevin Isom <kevin.isom@gmail.com>
2018-09-17 10:22:15 +1200

    change db connection info

--- usage/usageapp/settings.js
+++ usage/usageapp/settings.js
@@ -1,5 +1,6 @@
 module.exports = {
   NAME: 'ToDo - Anga Example',
   INSTALLED_APPS: ['anga-admin', 'anga-users', 'anga-mailer', 'app'],
-  CONNECTION: 'mongodb://127.0.0.1:27017/anga'
+  CONNECTION: 'mongodb://127.0.0.1:27017',
+  DB: 'anga'
 };
