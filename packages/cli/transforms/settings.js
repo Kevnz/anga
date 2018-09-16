9549b3c65c13106a303384debb49ac2bdb4ee510
Kevin Isom <kevin.isom@gmail.com>
2018-09-16 23:23:50 +1200

    code style

--- packages/cli/transforms/settings.js
+++ packages/cli/transforms/settings.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const program = require('ast-query');
 const prettier = require('prettier');
 
-module.exports = async (name) => {
+module.exports = async name => {
   const source = await fs.readFile('./settings.js', 'utf-8');
   const tree = program(source);
   const n = tree.assignment('module.exports');
